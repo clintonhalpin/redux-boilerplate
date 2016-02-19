4136d50cf80c23ca57a717ca94a24a213f6ba939
Clinton Halpin <clintonhalpin@gmail.com>
2016-02-18 23:52:00 -0500

    Remove console

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,6 @@ import React, { Component, PropTypes } from 'react';
 
 export class App extends Component {
   render() {
-    console.log(this.props);
     const { children } = this.props
     return (
       { children }
