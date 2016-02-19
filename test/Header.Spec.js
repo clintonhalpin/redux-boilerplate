d97268384e3891727b6bff28d3710b26ac3cd7c1
Clinton Halpin <clintonhalpin@gmail.com>
2016-02-18 22:15:30 -0500

    Fix tests and add test for header

---
+++ test/Header.Spec.js
@@ -0,0 +1,34 @@
+import 'babel-polyfill';
+import expect from 'expect';
+import expectJSX from 'expect-jsx';
+expect.extend(expectJSX);
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import Header from './../src/components/Header';
+
+function setup(props) {
+  let renderer = TestUtils.createRenderer()
+  renderer.render(<Header />)
+  let output = renderer.getRenderOutput()
+
+  return {
+    output,
+    renderer
+  }
+}
+
+describe('components', () => {
+  describe('Header', () => {
+    it('should render', () => {
+      const { output } = setup();
+      expect(output.type).toBe('div');
+    })
+    it('should dynamically set a title', () => {
+      let renderer = TestUtils.createRenderer()
+      renderer.render(<Header title='Fun' />)
+      let output = renderer.getRenderOutput()
+      let expected = 'Fun';
+      expect(output).toIncludeJSX('Fun');
+    })
+  })
+})
